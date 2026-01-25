6abda50b3d7182eae751a2d08b70c6aa4a575e8b
awanali <aliim.wanali@gmail.com>
2026-01-25 11:47:45 +0100

    Replaced Grab interaction with touch grab interaction

---
+++ audio-haeptics-ar_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,51 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void CollisionSoundPlayer_Start_m431FC32678D6AAB626645319E231CCBD9A975E6B (void);
+extern void CollisionSoundPlayer_OnCollisionEnter_mB53A0586F574A86AB185B5BFCF20C990E7A48A1B (void);
+extern void CollisionSoundPlayer__ctor_m2E8FCA561FDAFB7D0BCED93BC539185D13B18F6B (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	CollisionSoundPlayer_Start_m431FC32678D6AAB626645319E231CCBD9A975E6B,
+	CollisionSoundPlayer_OnCollisionEnter_mB53A0586F574A86AB185B5BFCF20C990E7A48A1B,
+	CollisionSoundPlayer__ctor_m2E8FCA561FDAFB7D0BCED93BC539185D13B18F6B,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	22103,
+	16895,
+	22103,
+	35540,
+	22103,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
