9a58ab1d874af50024ee739821f6709e51c2ab1a
awanali <aliim.wanali@gmail.com>
2026-02-10 17:39:36 +0100

    Fixed some naming bugs and log orders

--- Assets/Scripts/TutorialView.cs
+++ Assets/Scripts/TutorialView.cs
@@ -4,12 +4,14 @@ public class TutorialView : View
 
     public override void Show()
     {
+        if (gameObject.activeSelf) return;
         base.Show();
         DataManager.Instance.Log("tutorialStartTimestamp", System.DateTime.Now.ToString("o"));
     }
 
     public override void Hide()
     {
+        if (!gameObject.activeSelf) return;
         base.Hide();
         DataManager.Instance.Log("tutorialEndTimestamp", System.DateTime.Now.ToString("o"));
     }
